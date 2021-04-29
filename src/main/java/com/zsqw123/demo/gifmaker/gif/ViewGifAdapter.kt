be60b660287a446c8f4528c510f3f4814df412ca
damyjy <zsqw123@vip.qq.com>
2021-04-30 00:33:51 +0800

    - add share

--- src/main/java/com/zsqw123/demo/gifmaker/gif/ViewGifAdapter.kt
+++ src/main/java/com/zsqw123/demo/gifmaker/gif/ViewGifAdapter.kt
@@ -2,6 +2,7 @@ package com.zsqw123.demo.gifmaker.gif
 
 import android.graphics.Bitmap
 import android.view.View
+import androidx.core.graphics.scale
 import androidx.core.view.drawToBitmap
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -9,12 +10,13 @@ import java.io.File
 
 class ViewGifAdapter(private val output: File, private val fps: Int, private val view: View) {
     /** @param count Int second */
-    suspend fun generate(count: Int, progressCallback: (Int) -> Unit) {
+    suspend fun generate(count: Int, outputSize: Int = 200, progressCallback: (Int) -> Unit) {
         withContext(Dispatchers.IO) {
             val encoder = GifEncoder(output)
             encoder.setFrameRate(fps)
             for (i in 1..count) {
-                encoder.addFrame(view.drawToBitmap(Bitmap.Config.RGB_565))
+                val origin = view.drawToBitmap(Bitmap.Config.RGB_565)
+                encoder.addFrame(origin.scale(width = outputSize, (outputSize.toFloat() / origin.width * origin.height).toInt()))
                 progressCallback(((i / count.toFloat()) * 100).toInt())
             }
             encoder.finish()
